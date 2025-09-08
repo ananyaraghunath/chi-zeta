02fff170dc7071c6f33aa560da033b41cd6fea67
Ananya Raghunath <ar2764@njit.edu>
2025-09-08 16:13:04 -0400

    Fix lazy loading and fix linkedin urls

--- scripts/loadBrothers.js
+++ scripts/loadBrothers.js
@@ -2,19 +2,20 @@
 ---
 
 //RUNS ON LOAD
+// RUNS ON LOAD
 loadJSON(function(response) {
   // Parse JSON string into object
   var brothers = JSON.parse(response);
   displayBrotherData(brothers);
- });
+});
 
 function loadJSON(callback) {
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
   xobj.open('GET', '/scripts/brotherdata.json', true);
   xobj.onreadystatechange = function () {
-    if (xobj.readyState == 4 && xobj.status == "200") {
-      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+    if (xobj.readyState === 4 && xobj.status === 200) {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in async mode
       callback(xobj.responseText);
     }
   };
@@ -24,15 +25,16 @@ function loadJSON(callback) {
 function displayBrotherData(dictionary) {
   var html = "";
   for (var semester in dictionary) {
-    html += "<div class='container'><h2>"+semester+"</h2><hr><div class='grid'>"
+    html += "<div class='container'><h2>" + semester + "</h2><hr><div class='grid'>";
     for (var name in dictionary[semester]) {
-      if (dictionary[semester][name]['data']) {
-        html += "<div><img data-collective="+dictionary[semester][name]['data']+" src='"+dictionary[semester][name]['headshot']+"'/><p>"+name+"</p></div>"
+      var brother = dictionary[semester][name];
+      if (brother['data']) {
+        html += "<div><img data-collective='" + brother['data'] + "' loading='lazy' src='" + brother['headshot'] + "'/><p>" + name + "</p></div>";
       } else {
-        html += "<div><a href='//"+dictionary[semester][name]['linkedin']+"'><img src='"+dictionary[semester][name]['headshot']+"'/></a><p>"+name+"</p></div>"
+        html += "<div><a href='//" + brother['linkedin'] + "'><img loading='lazy' src='" + brother['headshot'] + "'/></a><p>" + name + "</p></div>";
       }
     }
-    html += "</div></div></div>"
+    html += "</div></div>";
   }
   document.getElementById("headshots").innerHTML = html;
   loadCollective();
